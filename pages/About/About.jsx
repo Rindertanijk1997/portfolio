a95f4f6f6e519c6fde56343b0978c6a3c78d518c
Rindertanijk1997 <yhfe23.van.ri@folkuniversitetet.nu>
2024-07-26 16:21:06 +0200

    skills fix

--- pages/About/About.jsx
+++ pages/About/About.jsx
@@ -4,8 +4,32 @@ import './about.css'
 function About() {
   return (
     <section className='about'>
-      <h1 className='about_h1'>About</h1>
+
+      <section className='about_h1'>About</section>
+
+      <section className='about_text'>
+        <h2 className='about_h2'>Jag heter Rindert van Dijk, är 27 år gammal och bor i vackra Karlstad.
+          Ursprungligen kommer jag från Nederländerna,
+          men flyttade till Sverige när jag var ung.
+          För närvarande studerar jag till frontend-utvecklare,
+          en utbildning som jag tycker är både utmanande och spännande.
+
+          När jag inte är upptagen med mina studier,
+          tycker jag om att spela golf. Det finns få saker som är så avkopplande
+          som en dag på golfbanan. Jag gillar också att umgås med mina vänner,
+          vare sig det handlar om att ta en fika på stan,
+          spela sällskapsspel eller njuta av en trevlig middag.
+
+          Att resa när tillfälle ges är en annan passion jag har;
+          jag älskar att utforska nya platser och kulturer.
+        </h2>
+      </section>
+      <section className='about_img'>
+      <img src="src/assets/jag.png" alt="bild på mig" />
+      </section>
+
     </section>
+
   )
 }
 
--- pages/Skills/Skills.jsx
+++ pages/Skills/Skills.jsx
@@ -1,8 +1,94 @@
 import React from 'react'
+import './skills.css'
 
 function Skills() {
   return (
-    <div>Skills</div>
+    <section className='skills'>
+
+    <section className='skills_h1'>Skills</section>
+
+    <section className='skills_left'>
+    <ul>
+  <li>
+    <div className="skill-icon">
+      <div className="skill-icon-inner"></div>
+    </div>
+    Arbeta agilt
+  </li>
+  <li>
+    <div className="skill-icon">
+      <div className="skill-icon-inner"></div>
+    </div>
+    Kan jobba både i team och själv
+  </li>
+  <li>
+    <div className="skill-icon">
+      <div className="skill-icon-inner"></div>
+    </div>
+    Bra tidsuppfattning
+  </li>
+  <li>
+    <div className="skill-icon">
+      <div className="skill-icon-inner"></div>
+    </div>
+    text
+  </li>
+  <li>
+    <div className="skill-icon">
+      <div className="skill-icon-inner"></div>
+    </div>
+    text
+  </li>
+  <li>
+    <div className="skill-icon">
+      <div className="skill-icon-inner"></div>
+    </div>
+    text
+  </li>
+</ul>
+    </section>
+    <section className='skills_right'>
+  <ul>
+    <li>
+      HTML
+      <div className="skill-bar">
+        <div className="skill-level" style={{width: '50%'}}></div>
+      </div>
+    </li>
+    <li>
+      CSS
+      <div className="skill-bar">
+        <div className="skill-level" style={{width: '40%'}}></div>
+      </div>
+    </li>
+    <li>
+      UX/UI
+      <div className="skill-bar">
+        <div className="skill-level" style={{width: '30%'}}></div>
+      </div>
+    </li>
+    <li>
+      JAVASCRIPT
+      <div className="skill-bar">
+        <div className="skill-level" style={{width: '30%'}}></div>
+      </div>
+    </li>
+    <li>
+      REACT
+      <div className="skill-bar">
+        <div className="skill-level" style={{width: '40%'}}></div>
+      </div>
+    </li>
+    <li>
+      NODE
+      <div className="skill-bar">
+        <div className="skill-level" style={{width: '35%'}}></div>
+      </div>
+    </li>
+  </ul>
+</section>
+
+  </section>
   )
 }
 
