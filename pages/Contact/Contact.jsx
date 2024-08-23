7a70dfb51e8c8f435f029d8690aba8cc233ed576
Rindert van Dijk <143588734+Rindertanijk1997@users.noreply.github.com>
2024-08-23 14:49:57 +0200

    Revert "Dev"

--- pages/Contact/Contact.jsx
+++ pages/Contact/Contact.jsx
@@ -1,31 +1,8 @@
 import React from 'react'
-import './contact.css'
 
 function Contact() {
   return (
-    <section className='contact'>
-      <h1 className='contact-h1'>Kontakt</h1>
-      <section className='contact-uppgifter'>
-        <section className='contact-uppgifter-telefon'>
-          <h2>Telefon:</h2>
-          <p>0730205483</p>
-        </section>
-        <section className='contact-uppgifter-linkedin'>
-          <a href="https://www.linkedin.com/in/rindertvandijk" target="_blank">
-            Besök min LinkedIn
-            <img src="src/assets/linkedin.png" alt="LinkedIn Logo" class="linkedin-logo" />
-          </a>
-        </section>
-        <section className='contact-uppgifter-mail'>
-          <a href="mailto:rindert.vandijk@hotmail.se?subject=Feedback från webbsidan&body=Hej, jag vill gärna komma i kontakt med dig angående...">
-            Skicka Mail 
-            <img src="src/assets/epostlogo.png" alt="Epost Logo" class="email-logo" />
-          </a>
-        </section>
-      </section>
-
-
-    </section>
+    <div>Contact</div>
   )
 }
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
-import { useState } from 'react';
-import './App.css';
-import About from '../pages/About/About';
-import Contact from '../pages/Contact/Contact';
-import Experiences from '../pages/Experiences/Experiences'
-import Skills from '../pages/Skills/Skills';
-import Homepage from '../pages/Homepage/Homepage';
-import Header from '../Components/Header/Header';
+import { useState } from 'react'
+import './App.css'
+import About from '../pages/About/About'
+import Contact from '../pages/Contact/Contact'
+import Projects from '../pages/Project/Projects'
+import Skills from '../pages/Skills/Skills'
+import Homepage from '../pages/Homepage/Homepage'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
-    <div>
-    <Header />
-    <div id="home"><Homepage /></div>
-    <div id="about"><About /></div>
-    <div id="experiences"><Experiences /></div>
-    <div id="skills"><Skills /></div>
-    <div id="contact"><Contact /></div>
-  </div>
-  );
+    <Router>
+      <>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/skills" element={<Skills />} />
+        </Routes>
+      </>
+    </Router>
+  )
 }
 
-export default App;
+export default App
